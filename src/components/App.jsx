b7d0b877920bcaad39fb2cc9155873da0324072b
Rosario Tarabocchia <84477640+rosario-tarabocchia@users.noreply.github.com>
2022-04-18 11:28:56 -0400

    Lesson Complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+
+  // function handleFirstNameChange(event) {
+  //   setFirstName(event.target.value);
+  // }
+
+  // function handleLastNameChange(event) {
+  //   setLastName(event.target.value);
+  // }
+
+  const [fullName, setFullName] = useState({
+    firstName: "",
+    lastName: ""
+  });
+
+  function handleChange(event) {
+    // const newValue = event.target.value;
+    // const inputName = event.target.name;
+
+    const { value, name } = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          firstName: value,
+          lastName: prevValue.lastName
+        };
+      } else if (name === "lastName") {
+        return {
+          firstName: prevValue.firstName,
+          lastName: value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.firstName} {fullName.lastName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.firstName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lastName}
+        />
         <button>Submit</button>
       </form>
     </div>
